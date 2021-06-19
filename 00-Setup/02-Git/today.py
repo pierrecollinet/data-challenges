1628e9f187be9a59d84a40af62bb5b71e8988457
pierre collinet <pi.collinet@gmail.com>
2021-06-19 15:17:13 +0200

    Fix style issues, should get a 'Good Style' now :pray:

--- 00-Setup/02-Git/today.py
+++ 00-Setup/02-Git/today.py
@@ -1,2 +1,8 @@
+"""A module computing buddy pair names for the day"""
 def my_name_is():
+    """function docstring..."""
     return "pierrecollinet"
+
+def my_buddy_is():
+    """function docstring..."""
+    return "nobody"
